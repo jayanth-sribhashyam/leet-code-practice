fed9d1ef8fa4244b8a6dea0be87b0f3dccad75d2
Jayanth <129029085+jayanth-sribhashyam@users.noreply.github.com>
2023-04-08 14:40:29 +0530

    Create alternatingbinarybits.py

---
+++ alternatingbinarybits.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def hasAlternatingBits(self, n):
+        binary=bin(n)[2:]
+
+        for i in range(len(binary)-1):
+            if binary[i]==binary[i+1]:
+                return False
+        
+        return True
